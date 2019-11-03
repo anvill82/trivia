c0278af85dc51ee5a5ae6ff42a0e8a7ac1c4d40a
ivailo <ivailo.s.stanev@gmail.com>
2019-11-03 22:06:55 +0200

    Added several more stupid tests

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -26,66 +26,116 @@ public class Game {
     	}
     }
 
-	public String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
-	
-	public boolean isPlayable() {
-		return (howManyPlayers() >= 2);
-	}
-
 	public boolean add(String playerName) {
-		
-		
-	    players.add(playerName);
-	    places[howManyPlayers()] = 0;
-	    purses[howManyPlayers()] = 0;
-	    inPenaltyBox[howManyPlayers()] = false;
-	    
-	    System.out.println(playerName + " was added");
-	    System.out.println("They are player number " + players.size());
+
+
+		players.add(playerName);
+		places[howManyPlayers()] = 0;
+		purses[howManyPlayers()] = 0;
+		inPenaltyBox[howManyPlayers()] = false;
+
+		System.out.println(playerName + " was added");
+		System.out.println("They are player number " + players.size());
 		return true;
-	}
-	
-	public int howManyPlayers() {
-		return players.size();
 	}
 
 	public void roll(int roll) {
 		System.out.println(players.get(currentPlayer) + " is the current player");
 		System.out.println("They have rolled a " + roll);
-		
+
 		if (inPenaltyBox[currentPlayer]) {
 			if (roll % 2 != 0) {
 				isGettingOutOfPenaltyBox = true;
-				
+
 				System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
 				places[currentPlayer] = places[currentPlayer] + roll;
 				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-				
-				System.out.println(players.get(currentPlayer) 
-						+ "'s new location is " 
+
+				System.out.println(players.get(currentPlayer)
+						+ "'s new location is "
 						+ places[currentPlayer]);
 				System.out.println("The category is " + currentCategory());
 				askQuestion();
 			} else {
 				System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
 				isGettingOutOfPenaltyBox = false;
-				}
-			
+			}
+
 		} else {
-		
+
 			places[currentPlayer] = places[currentPlayer] + roll;
 			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-			
-			System.out.println(players.get(currentPlayer) 
-					+ "'s new location is " 
+
+			System.out.println(players.get(currentPlayer)
+					+ "'s new location is "
 					+ places[currentPlayer]);
 			System.out.println("The category is " + currentCategory());
 			askQuestion();
 		}
-		
 	}
+
+	public boolean isPlayable() {
+		return (howManyPlayers() >= 2);
+	}
+
+	public String createRockQuestion(int index){
+		return "Rock Question " + index;
+	}
+
+	public int howManyPlayers() {
+		return players.size();
+	}
+
+	public boolean wasCorrectlyAnswered() {
+		if (inPenaltyBox[currentPlayer]){
+			if (isGettingOutOfPenaltyBox) {
+				System.out.println("Answer was correct!!!!");
+				purses[currentPlayer]++;
+				System.out.println(players.get(currentPlayer)
+						+ " now has "
+						+ purses[currentPlayer]
+						+ " Gold Coins.");
+
+				boolean winner = didPlayerWin();
+				currentPlayer++;
+				if (currentPlayer == players.size()) currentPlayer = 0;
+
+				return winner;
+			} else {
+				currentPlayer++;
+				if (currentPlayer == players.size()) currentPlayer = 0;
+				return true;
+			}
+
+
+
+		} else {
+
+			System.out.println("Answer was corrent!!!!");
+			purses[currentPlayer]++;
+			System.out.println(players.get(currentPlayer)
+					+ " now has "
+					+ purses[currentPlayer]
+					+ " Gold Coins.");
+
+			boolean winner = didPlayerWin();
+			currentPlayer++;
+			if (currentPlayer == players.size()) currentPlayer = 0;
+
+			return winner;
+		}
+	}
+
+	public boolean wrongAnswer(){
+		System.out.println("Question was incorrectly answered");
+		System.out.println(players.get(currentPlayer)+ " was sent to the penalty box");
+		inPenaltyBox[currentPlayer] = true;
+
+		currentPlayer++;
+		if (currentPlayer == players.size()) currentPlayer = 0;
+		return true;
+	}
+
 
 	private void askQuestion() {
 		if (currentCategory() == "Pop")
@@ -95,10 +145,8 @@ public class Game {
 		if (currentCategory() == "Sports")
 			System.out.println(sportsQuestions.removeFirst());
 		if (currentCategory() == "Rock")
-			System.out.println(rockQuestions.removeFirst());		
+			System.out.println(rockQuestions.removeFirst());
 	}
-	
-	
 	private String currentCategory() {
 		if (places[currentPlayer] == 0) return "Pop";
 		if (places[currentPlayer] == 4) return "Pop";
@@ -111,58 +159,6 @@ public class Game {
 		if (places[currentPlayer] == 10) return "Sports";
 		return "Rock";
 	}
-
-	public boolean wasCorrectlyAnswered() {
-		if (inPenaltyBox[currentPlayer]){
-			if (isGettingOutOfPenaltyBox) {
-				System.out.println("Answer was correct!!!!");
-				purses[currentPlayer]++;
-				System.out.println(players.get(currentPlayer) 
-						+ " now has "
-						+ purses[currentPlayer]
-						+ " Gold Coins.");
-				
-				boolean winner = didPlayerWin();
-				currentPlayer++;
-				if (currentPlayer == players.size()) currentPlayer = 0;
-				
-				return winner;
-			} else {
-				currentPlayer++;
-				if (currentPlayer == players.size()) currentPlayer = 0;
-				return true;
-			}
-			
-			
-			
-		} else {
-		
-			System.out.println("Answer was corrent!!!!");
-			purses[currentPlayer]++;
-			System.out.println(players.get(currentPlayer) 
-					+ " now has "
-					+ purses[currentPlayer]
-					+ " Gold Coins.");
-			
-			boolean winner = didPlayerWin();
-			currentPlayer++;
-			if (currentPlayer == players.size()) currentPlayer = 0;
-			
-			return winner;
-		}
-	}
-	
-	public boolean wrongAnswer(){
-		System.out.println("Question was incorrectly answered");
-		System.out.println(players.get(currentPlayer)+ " was sent to the penalty box");
-		inPenaltyBox[currentPlayer] = true;
-		
-		currentPlayer++;
-		if (currentPlayer == players.size()) currentPlayer = 0;
-		return true;
-	}
-
-
 	private boolean didPlayerWin() {
 		return !(purses[currentPlayer] == 6);
 	}
--- java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
@@ -7,74 +7,161 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class GameTest {
 
-	public static final String testPlayerName = "TestPlayer";
-	private ByteArrayOutputStream stream;
+    public static final String testPlayerName = "TestPlayer";
+    private ByteArrayOutputStream stream;
 
-	@Before
-	public void getOutputStreamFromConsole() {
-		stream = new ByteArrayOutputStream();
-		PrintStream printStream = new PrintStream(stream);
-		System.setOut(printStream);
-	}
+    @Before
+    public void getOutputStreamFromConsole() {
+        stream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(stream);
+        System.setOut(printStream);
+    }
 
-	@Test
-	public void whenGameIsCreatedStreamIsEmpty() {
+    @Test
+    public void whenGameIsCreatedStreamIsEmpty() {
+        Game game = new Game();
+        assertEquals("", stream.toString());
+    }
+
+    @Test
+    public void whenPlayerIsAddedHisNameIsWritten() {
+        Game game = new Game();
+        game.add("TestPlayer");
+        assertEquals("TestPlayer was added\n" +
+                "They are player number 1\n", getStreamFromGame());
+    }
+
+    @Test
+    public void whenGameCreatedWithOnePlayerTotalPlayersIsOne() {
+        Game game = new Game();
+        game.add(testPlayerName);
+        assertEquals(1, game.howManyPlayers());
+    }
+
+    @Test
+    public void whenGameCreatedWithMoreThanSixPlayersExceptionIsThrown() {
+        Game game = new Game();
+
+        try {
+            for (int i = 0; i < 7; i++) {
+                game.add(testPlayerName);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            assertThat(e.getMessage(), containsString("6"));
+        }
+    }
+
+    @Test
+    public void whenOnePlayerIsAddedGameIsNotPlayable() {
+        Game game = new Game();
+        game.add(testPlayerName);
+        assertEquals(false, game.isPlayable());
+    }
+
+    @Test
+    public void whenTwoPlayersAreAddedGameIsPlayable() {
+        Game game = new Game();
+        game.add(testPlayerName);
+        game.add(testPlayerName);
+        assertEquals(true, game.isPlayable());
+    }
+
+    @Test
+    public void gameAdd() {
+        Game game = new Game();
+    }
+
+    @Test
+    public void gameRollOne() {
 		Game game = new Game();
-		assertEquals("", stream.toString());
-	}
-
-	@Test
-	public void whenPlayerIsAddedHisNameIsWritten() {
-		Game game = new Game();
-		game.add("TestPlayer");
+		game.add(testPlayerName);
+		game.roll(1);
 		assertEquals("TestPlayer was added\n" +
-				"They are player number 1\n", stream.toString().replaceAll("\r", ""));
-	}
+				"They are player number 1\n" +
+				"TestPlayer is the current player\n" +
+				"They have rolled a 1\n" +
+				"TestPlayer's new location is 1\n" +
+				"The category is Science\n" +
+				"Science Question 0\n", getStreamFromGame());
+    }
 
-	@Test
-	public void whenGameCreatedWithOnePlayerTotalPlayersIsOne() {
+    @Test
+    public void gameRollTwo() {
 		Game game = new Game();
 		game.add(testPlayerName);
-		assertEquals(1, game.howManyPlayers());
-	}
+		game.roll(2);
+		assertEquals("TestPlayer was added\n" +
+				"They are player number 1\n" +
+				"TestPlayer is the current player\n" +
+				"They have rolled a 2\n" +
+				"TestPlayer's new location is 2\n" +
+				"The category is Sports\n" +
+				"Sports Question 0\n", getStreamFromGame());
+    }
 
-	@Test
-	public void whenGameCreatedWithMoreThanSixPlayersExceptionIsThrown() {
-		Game game = new Game();
-
-		try {
-			for (int i = 0; i < 7; i++) {
-				game.add(testPlayerName);
-			}
-		} catch (ArrayIndexOutOfBoundsException e) {
-			assertThat(e.getMessage(), containsString("6"));
-		}
-	}
-
-	@Test
-	public void whenOnePlayerIsAddedGameIsNotPlayable() {
+    @Test
+    public void gameRollThree() {
 		Game game = new Game();
 		game.add(testPlayerName);
-		assertEquals(false, game.isPlayable());
-	}
+		game.roll(3);
+		assertEquals("TestPlayer was added\n" +
+				"They are player number 1\n" +
+				"TestPlayer is the current player\n" +
+				"They have rolled a 3\n" +
+				"TestPlayer's new location is 3\n" +
+				"The category is Rock\n" +
+				"Rock Question 0\n", getStreamFromGame());
+    }
 
 	@Test
-	public void whenTwoPlayersAreAddedGameIsPlayable() {
-		Game game = new Game();
-		game.add(testPlayerName);
-		game.add(testPlayerName);
-		assertEquals(true, game.isPlayable());
-	}
+    public void gameAskQuestion() {
+		fail();
+    }
 
-	@Test
-	public void t1() {
-		Game game = new Game();
+    @Test
+    public void gameCreateRockQuestion() {
+        Game game = new Game();
+        //game.add(testPlayerName);
+        //game.roll(1);
+        game.createRockQuestion(1);
 
+        assertEquals("Rock Question 1", getStreamFromGame());
+        // Rock Question " + index
+    }
+
+
+
+    @Test
+    public void gameDidPlayerWin() {
+		fail();
+    }
+
+    @Test
+    public void gameHowManyPlayers() {
+		fail();
+    }
+
+    @Test
+    public void gameIsPlayable() {
+		fail();
+    }
+
+    @Test
+    public void gameWasCorrectlyAnswered() {
+		fail();
+    }
+
+    @Test
+    public void gameWrongAnswer() {
+        fail();
+    }
+
+	private String getStreamFromGame() {
+		return stream.toString().replaceAll("\r", "");
 	}
 }
